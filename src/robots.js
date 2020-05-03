a6e15ce86e050996c7f3f6a8d3a59d1273d86e6a
Himanshu Srivastav <himanshumagic41297@gmail.com>
2020-05-03 13:46:38 +0530

    initial commit

---
+++ src/robots.js
@@ -0,0 +1,88 @@
+export const robots = [
+    {
+      id: 100,
+      name: 'Leanne Graham',
+      username: 'Bret',
+      email: 'Sincere@april.biz'
+    },
+    {
+      id: 222,
+      name: 'Ervin Howell',
+      username: 'Antonette',
+      email: 'Shanna@melissa.tv'
+    },
+    {
+      id: 312,
+      name: 'Clementine Bauch',
+      username: 'Samantha',
+      email: 'Nathan@yesenia.net'
+    },
+    {
+      id: 441,
+      name: 'Patricia Lebsack',
+      username: 'Karianne',
+      email: 'Julianne.OConner@kory.org'
+    },
+    {
+      id: 552,
+      name: 'Chelsey Dietrich',
+      username: 'Kamren',
+      email: 'Lucio_Hettinger@annie.ca'
+    },
+    {
+      id: 674,
+      name: 'Mrs. Dennis Schulist',
+      username: 'Leopoldo_Corkery',
+      email: 'Karley_Dach@jasper.info'
+    },
+    {
+      id: 796,
+      name: 'Kurtis Weissnat',
+      username: 'Elwyn.Skiles',
+      email: 'Telly.Hoeger@billy.biz'
+    },
+    {
+      id: 878,
+      name: 'Nicholas Runolfsdottir V',
+      username: 'Maxime_Nienow',
+      email: 'Sherwood@rosamond.me'
+    },
+    {
+      id: 963,
+      name: 'Glenna Reichert',
+      username: 'Delphine',
+      email: 'Chaim_McDermott@dana.io'
+    },
+    {
+      id: 1045,
+      name: 'Clementina DuBuque',
+      username: 'Moriah.Stanton',
+      email: 'Rey.Padberg@karina.biz'
+    },
+    {
+        id: 4525,
+        name: 'Adam DuBuque',
+        username: 'adamDu',
+        email: 'du.adam@karina.biz'
+      },
+      {
+        id: 5401,
+        name: 'DuBuque',
+        username: 'Moriah.ton',
+        email: 'Moriah@karina.biz'
+      },
+      {
+        id: 7755,
+        name: 'Devil_him',
+        username: 'magic7',
+        email: 'magic7him@karina.biz'
+      },
+      {
+        id: 5577,
+        name: 'AnkurSri',
+        username: 'ankur.sri',
+        email: 'AnkurSrivastava@karina.biz'
+      },
+  ];
+
+  export default robots;
